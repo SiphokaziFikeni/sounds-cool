01645754621ea38151cd9f0616f497ae2c49eea6
Unknown <dumisile.fikeni@gmail.com>
2018-04-17 14:49:01 +0200

    Song and SondDetails models

--- sounds-cool/sounds-cool/View/Song.swift
+++ sounds-cool/sounds-cool/View/Song.swift
@@ -1,1 +1,26 @@
-import Foundation
+import UIKit
+
+struct Song {
+    
+    var id: String
+    var title: String
+    var artist: String
+    var releaseYear: String
+    var album: String?
+    var albumImage: UIImage?
+    
+    init?(id: String, title: String, artist: String, releaseYear: String, album: String?, albumImage: UIImage?) {
+        
+        if id.isEmpty || title.isEmpty || artist.isEmpty || releaseYear.isEmpty {
+            return nil
+        }
+        
+        self.id = id
+        self.title = title
+        self.artist = artist
+        self.releaseYear = releaseYear
+        self.album = album
+        self.albumImage = albumImage
+    }
+    
+}
